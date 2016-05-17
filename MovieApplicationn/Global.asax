97009bc3cfec110ffe2a4120f37a4b9d7adf3dfd
Your Name <you.example.com>
2016-05-16 20:34:55 -0400

    Submitting Movie Applicationn

---
+++ MovieApplicationn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieApplicationn.MvcApplication" Language="C#" %>
---
+++ MovieApplicationn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieApplicationn.Startup))]
+namespace MovieApplicationn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
